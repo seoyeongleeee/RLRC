425090ce6ff06e949dab4e661920f783aa3b0d87
seoyeongleeee <161130362+seoyeongleeee@users.noreply.github.com>
2025-04-15 14:07:42 +0900

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -23,4 +23,4 @@ if uploaded_file is not None:
         final_result = "정상" if result['normal'] > result['stroke'] else "뇌졸중"
         st.markdown(f"### 🔍 최종 판별: **{final_result}** 입니다.")
 
-        # test redeploy
+        # Streamlit redeploy test
